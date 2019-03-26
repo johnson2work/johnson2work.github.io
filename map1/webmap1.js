05274340e9211f46c08b00537e55ecfcb24d081a
johnson2work <48488067+johnson2work@users.noreply.github.com>
2019-03-26 00:04:08 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -6,6 +6,12 @@ let myTriangle = L.polygon([
   [30.066, -90.479],
   [30.089, -89.869],
 ]).addTo(myMap);
-
+var latlngs = [
+    [30.611163, -96.340136],
+    [30.412379, -91.183822],
+    [29.651222, -82.348364]
+];
+var polyline = L.polyline(latlngs, {color: 'red'}).addTo(myMap);
+map.fitBounds(polyline.getBounds());
 myTriangle.bindPopup('A polygon')
 nolaPoint.bindPopup('A <em>marker</em>')

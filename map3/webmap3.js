8ae6abc3a1ac1ea6f86a19242cd7de7b90085510
Yi-Pin C Johnson <yipin@lsu.edu>
2019-04-04 11:03:48 -0500

    class edit

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,2 +1,6 @@
-let myMap = L.map('map2').setView([39, -97.5], 4)
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
+let stateMap = L.map('map3').setView([39, -97.5], 4)
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(stateMap)
+let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
+jQuery.getJSON(stateDemographicsUrl, function (data) {
+  L.geoJSON(data).addTo(stateMap)
+})

191a2bf71ee7e42a5497d8558e2edead5b6af59d
johnson2work <48488067+johnson2work@users.noreply.github.com>
2019-04-25 10:32:26 -0500

    Add files via upload

---
+++ storymap.js
@@ -0,0 +1,17 @@
+let myMap = L.map('map1').setView([32.18, -99.14], 4)
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
+let nolaPoint = L.marker([30, -90]).addTo(myMap)
+let myTriangle = L.polygon([
+  [30.4706, -90.094],
+  [30.066, -90.479],
+  [30.089, -89.869],
+]).addTo(myMap);
+var latlngs = [
+    [30.611163, -96.340136],
+    [30.412379, -91.183822],
+    [29.651222, -82.348364]
+];
+var polyline = L.polyline(latlngs, {color: 'red'}).addTo(myMap);
+myMap.fitBounds(polyline.getBounds());
+myTriangle.bindPopup('A polygon')
+nolaPoint.bindPopup('A <em>marker</em>')
